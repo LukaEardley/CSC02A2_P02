4803a42fd2881a4e7190260ac29891fb9dfc9285
Luka Eardley <lukaeardley01@gmail.com>
2021-03-05 13:18:05 +0200

    Added the missing txt files to be looped through

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
 import file.DroneFileHandler;
 import model.Drones;
-import model.Sensor;
 
-import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
         //TODO
-        List<Drones> dronesList = DroneFileHandler.readDrones("corrupt.transmission");
+        List<Drones> dronesList = Stream.of(DroneFileHandler.readDrones("corrupt.transmission"), DroneFileHandler.readDrones("large.transmission"), DroneFileHandler.readDrones("normal.transmission")).filter(Objects::nonNull)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
         for (Drones drone : dronesList) {
             drone.displayDrones();
         }
